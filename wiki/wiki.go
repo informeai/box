992b4e5bd94b10381acdc66743349118e6907cb2
Wellington Gadelha <wellingtongadelha@Informeai.local>
2021-03-21 16:44:55 -0300

    adition wiki

---
+++ wiki/wiki.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Use: wiki -l <language> -w <word-search>")
+	}
+	lang := flag.String("l", "en", "language for search wikipedia")
+	word := flag.String("w", "wikipedia", "word for search")
+
+	flag.Parse()
+	url := string("https://" + *lang + ".wikipedia.org/wiki/" + *word)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(body)
+	}
+}
