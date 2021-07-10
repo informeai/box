2a637899943787257bb38c30256fbb33878e3c75
Wellington Gadelha <wellingtongadelha@Informeai.local>
2021-07-10 17:23:49 -0300

    Refactory code in convertmedia dir

--- convertmedia/convert.go
+++ convertmedia/convert.go
@@ -1,63 +1,96 @@
-package main
+package convertmedia
 
 import (
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
-
+	"errors"
 	"github.com/xfrr/goffmpeg/transcoder"
 )
 
-func main() {
+type MediaConverter interface{
+	verifyArgs() error
+	verifyFile() error
+	transcoderFile() error
+}
 
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: convert [input file] [output file]")
-		return
+type Convert struct{
+	Args []string
+
+
+}
+
+func NewConvert(args []string) *Convert{
+	return &Convert{Args:args}
+
+}
+
+func(c *Convert) verifyArgs() error{
+	if len(c.Args) < 3{
+		fmt.Println("Usage: convert [input file] [output directory]")
+		return errors.New("error: args not completed") 
+
 	}
-	inputFile := os.Args[1]
-	outputFile := os.Args[2]
 
-	if _, err := os.Stat(inputFile); os.IsNotExist(err) {
-		log.Fatalf("Archive: %v not existed.", filepath.Base(inputFile))
-		os.Exit(1)
+	return nil
+}
+func(c *Convert) verifyFile() error{
+	input := c.Args[1]
+	output := c.Args[2]
+	
+	if _, err := os.Stat(input); os.IsNotExist(err){
+		return errors.New("error: input file not existed")
 	}
 
-	if _, err := os.Stat(filepath.Dir(outputFile)); os.IsNotExist(err) {
-		err := os.Mkdir(filepath.Dir(outputFile), 0755)
-		if err != nil {
-			log.Fatalf("Not created directory: %v", filepath.Dir(outputFile))
-			os.Exit(2)
+	if _, err := os.Stat(filepath.Dir(output)); os.IsNotExist(err){
+		err = os.Mkdir(filepath.Dir(output), 0755)
+		if err != nil{
+			return errors.New("error: output directory not created")
 		}
-
 	}
+	return nil
+}
 
+func(c *Convert) transcoderFile() error{
 	trans := new(transcoder.Transcoder)
-
-	err := trans.Initialize(inputFile, outputFile)
-
-	if err != nil {
-		log.Fatalln(err)
+	err := trans.Initialize(c.Args[1],c.Args[2])
+	if err != nil{
+		return errors.New("error: transcoder not initialized")
 	}
-
 	duration := trans.MediaFile().DurationInput()
 	fmt.Println(duration)
 	done := trans.Run(true)
 
 	progress := trans.Output()
 	fmt.Printf("Initialized\n")
-	fmt.Printf("INPUT: %v\n", filepath.Base(inputFile))
-	fmt.Printf("OUTPUT: %v\n", filepath.Base(outputFile))
-	for p := range progress {
-		fmt.Printf("\rPROGRESS: %v\tSPEED: %v ", p.CurrentTime, p.Speed[:len(p.Speed)-1])
-	}
+	fmt.Printf("INPUT: %v\n", filepath.Base(c.Args[1]))
+	fmt.Printf("OUTPUT: %v\n", filepath.Base(c.Args[2]))
+	for p := range progress{
+		fmt.Printf("\rPROGRESS: %v\tSPEED: %v", p.CurrentTime, p.Speed[:len(p.Speed)-1])
 
-	err = <-done
-	if err != nil && err != io.EOF {
-		log.Fatalln(err.Error())
-		os.Exit(2)
-	} else {
-		fmt.Printf("\nFinished\n")
 	}
+	err = <- done
+	if err != nil && err != io.EOF{
+		return errors.New("error: problem in process of transcoder file")
+
+	}
+	fmt.Printf("\nFinished\n")
+	return nil
+}
+func(c *Convert) Convert() error{
+	err := c.verifyArgs()
+	if err != nil{
+		return err
+	}
+	err = c.verifyFile()
+	if err != nil{
+		return err
+	}
+	err = c.transcoderFile()
+	if err != nil{
+		return err
+	}
+	return nil
+
 }
